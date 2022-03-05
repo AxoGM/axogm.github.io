0b18b89b186bf790a5d8a51a3af0800692b04ef8
AxoGM <100815115+AxoGM@users.noreply.github.com>
2022-03-05 17:47:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,1 @@
-// Import dependencie
-const express = require("express");
-// Setup the express server
-const app = express();
-const port = 3000;
-// Import middlewares into express
-app.use(express.json({limit:"100mb"}));
-// Import routes
-const authRounter = require("./routers/auth");
-const messagesRouter = require("./routers/messages");
-// Setup all the routers
-app.use("/api/messages", messagesRouter);
-app.use("/api/auth", authRounter);
-// Start the server
-app.listen(port,() => {
-    console.log('Listening on port ${port}..')
-});
+function _0x4982(){const _0x49576d=['/api/messages','Listening\x20on\x20port\x20${port}..','130131pxHGZF','974552RPBVQW','1836210ZKNyGX','1393316PhLEVs','9tYKZje','654YufFYc','452lMiezw','use','6395320BVLnqM','express','1309AQQMrL','674lcbWIX','listen','./routers/auth'];_0x4982=function(){return _0x49576d;};return _0x4982();}const _0x1fb183=_0x381d;(function(_0x328a45,_0x15fc81){const _0x3e9720=_0x381d,_0x47b80d=_0x328a45();while(!![]){try{const _0x4e1a82=parseInt(_0x3e9720(0x115))/0x1*(parseInt(_0x3e9720(0x10a))/0x2)+parseInt(_0x3e9720(0x10f))/0x3+-parseInt(_0x3e9720(0x112))/0x4+-parseInt(_0x3e9720(0x111))/0x5+parseInt(_0x3e9720(0x114))/0x6*(-parseInt(_0x3e9720(0x109))/0x7)+parseInt(_0x3e9720(0x110))/0x8*(parseInt(_0x3e9720(0x113))/0x9)+parseInt(_0x3e9720(0x117))/0xa;if(_0x4e1a82===_0x15fc81)break;else _0x47b80d['push'](_0x47b80d['shift']());}catch(_0x1f7acb){_0x47b80d['push'](_0x47b80d['shift']());}}}(_0x4982,0x35faa));const express=require(_0x1fb183(0x118)),app=express(),port=0xbb8;function _0x381d(_0x27bdcb,_0x531247){const _0x498295=_0x4982();return _0x381d=function(_0x381d5f,_0x35058e){_0x381d5f=_0x381d5f-0x109;let _0x54a03f=_0x498295[_0x381d5f];return _0x54a03f;},_0x381d(_0x27bdcb,_0x531247);}app[_0x1fb183(0x116)](express['json']({'limit':'100mb'}));const authRounter=require(_0x1fb183(0x10c)),messagesRouter=require('./routers/messages');app['use'](_0x1fb183(0x10d),messagesRouter),app[_0x1fb183(0x116)]('/api/auth',authRounter),app[_0x1fb183(0x10b)](port,()=>{const _0x2c805a=_0x1fb183;console['log'](_0x2c805a(0x10e));});
